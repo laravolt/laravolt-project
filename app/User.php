335cbcd0494e80e07a7d819f90dd7097574c176c
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-05-09 07:29:13 +0700

    Apply AutoSort to User model

--- app/User.php
+++ app/User.php
@@ -4,8 +4,9 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Laravolt\Epicentrum\Models\User as Authenticatable;
+use Laravolt\Suitable\AutoSort;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, AutoSort;
 }
