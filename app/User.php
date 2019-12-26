77c67b6e3ddc1f7e0310fc02d0333b05fa006f73
anandia <anandiamy@gmail.com>
2019-12-26 17:29:03 +0700

    autosort and autofilter user model

--- app/User.php
+++ app/User.php
@@ -4,8 +4,12 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Laravolt\Platform\Models\User as Authenticatable;
+use Laravolt\Suitable\AutoFilter;
+use Laravolt\Suitable\AutoSort;
 
 class User extends Authenticatable
 {
+    use AutoSort;
+    use AutoFilter;
     use Notifiable;
 }
