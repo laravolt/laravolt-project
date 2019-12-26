6974ecebff9e8a1b09713f02560bc95efbe0d57a
Anandia Muhammad Yudhistira <anandiamy@gmail.com>
2019-12-26 12:46:53 +0700

    chang adminseeder to rootseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         app('laravolt.acl')->syncPermission();
-        $this->call(AdminSeeder::class);
+        $this->call(RootSeeder::class);
         $this->call(UserSeeder::class);
     }
 }
